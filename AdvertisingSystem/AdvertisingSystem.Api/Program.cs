b18562a62dda822fca07497c8db7a511d2555253
Mark Martin Kaposvari <61698307+Markesz7@users.noreply.github.com>
2022-10-29 21:32:49 +0200

    Add dbcontext to DI, add connection string config

--- AdvertisingSystem/AdvertisingSystem.Api/Program.cs
+++ AdvertisingSystem/AdvertisingSystem.Api/Program.cs
@@ -1,3 +1,6 @@
+using AdvertisingSystem.Dal;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(o =>
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
