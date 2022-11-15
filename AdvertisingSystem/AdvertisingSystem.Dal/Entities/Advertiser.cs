538bf7b091cd7c89628e3e1b0f4c9320cb38515e
Mark Martin Kaposvari <61698307+Markesz7@users.noreply.github.com>
2022-11-15 18:30:18 +0100

    clean up code

--- AdvertisingSystem/AdvertisingSystem.Dal/Entities/Advertiser.cs
+++ AdvertisingSystem/AdvertisingSystem.Dal/Entities/Advertiser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdvertisingSystem.Dal.Entities
+﻿namespace AdvertisingSystem.Dal.Entities
 {
     public class Advertiser : ApplicationUser
     {
